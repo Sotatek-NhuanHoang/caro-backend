08abf28bb1f614200f0a05f165fe7296d47c3986
nhuan.hoangduc@outlook.com <nhuan.hoangduc@outlook.com>
2019-03-04 23:48:05 +0700

    Done room screen ui

--- ClientServer/src/screens/RoomScreen/RoomScreen.js
+++ ClientServer/src/screens/RoomScreen/RoomScreen.js
@@ -1,5 +1,8 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+
+import RoomItem from './RoomItem';
 
 import './RoomScreen.scss';
 
@@ -9,7 +12,24 @@ class RoomScreen extends PureComponent {
     render() {
         return (
             <div id="room-screen">
-                room screenas dfasdf
+                <div className="container grid-sm">
+                    <div className="room-status-container">
+                        {/* New room button */}
+                        <button className="add-button">New room</button>
+
+                        {/* Total room status */}
+                        <span>Total room: 123</span>
+                    </div>
+
+                    {/* List rooms */}
+                    <table className="table table-hover">
+                        <tbody>
+                            {_.map(new Array(100), () => (
+                                <RoomItem />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
