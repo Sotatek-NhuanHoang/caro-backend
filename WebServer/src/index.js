ac8198e39ece829094c3ad5fc84da0da4e2510d1
Sotate-NhuanHoang <nhuan.hoang@sotatek.com>
2019-03-08 09:06:15 +0700

    Update version 1.1.2

--- WebServer/src/index.js
+++ WebServer/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-console.log('Version 1.1.1');
+console.log('Version 1.1.2');
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
